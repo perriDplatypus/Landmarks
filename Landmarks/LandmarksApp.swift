1ba85669fa86353b720c16f1b2a7927da901f336
abhay <abhaytyagi2513@gmail.com>
2023-11-04 22:43:20 +0530

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Abhay Tyagi on 4/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
